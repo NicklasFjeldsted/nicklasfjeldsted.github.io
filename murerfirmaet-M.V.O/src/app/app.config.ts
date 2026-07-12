cc0a2c4c966735de0731a50bdc732dc8463b4b87
Nicklas <nicklas@estatetool.dk>
2026-07-12 18:28:26 +0200

    add FaIconRegistryService and configure APP_INITIALIZER in appConfig

--- murerfirmaet-M.V.O/src/app/app.config.ts
+++ murerfirmaet-M.V.O/src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { FaIconRegistryService } from './core/services/fa-icon-registry.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (fa: FaIconRegistryService) => () => fa.register(),
+      deps: [FaIconRegistryService],
+      multi: true,
+    },
   ]
 };
---
+++ murerfirmaet-M.V.O/src/app/core/services/fa-icon-registry.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+@Injectable({ providedIn: 'root' })
+export class FaIconRegistryService {
+  private readonly library = inject(FaIconLibrary);
+
+  register(): void {
+    this.library.addIconPacks(fas, far, fab);
+  }
+}
+
