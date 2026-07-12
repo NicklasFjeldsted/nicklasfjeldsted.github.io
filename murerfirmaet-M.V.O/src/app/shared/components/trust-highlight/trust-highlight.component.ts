2188b0475a3a12dcb524ac6717619dd3c752ab38
Nicklas <nicklas@estatetool.dk>
2026-07-12 19:52:10 +0200

    add trust highlight component with input interface

---
+++ murerfirmaet-M.V.O/src/app/shared/components/trust-highlight/trust-highlight.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { ITrustHighlight } from '../../../core/interfaces/trust-highlight.interface';
+
+@Component({
+  selector: 'mvo-trust-highlight',
+  imports: [FaIconComponent],
+  templateUrl: './trust-highlight.component.html',
+  styleUrl: './trust-highlight.component.scss',
+  standalone: true
+})
+export class TrustHighlightComponent {
+  readonly highlight = input<ITrustHighlight>();
+}
