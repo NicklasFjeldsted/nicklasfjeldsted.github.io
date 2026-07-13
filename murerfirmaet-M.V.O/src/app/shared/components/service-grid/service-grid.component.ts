c6c4637783371d7eb03e958cdcfa87abf6624fc9
Nicklas <nicklas@estatetool.dk>
2026-07-13 02:29:15 +0200

    update service grid component with new images and formatting adjustments

--- murerfirmaet-M.V.O/src/app/shared/components/service-grid/service-grid.component.ts
+++ murerfirmaet-M.V.O/src/app/shared/components/service-grid/service-grid.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
-import { IProvidedService } from '../../../core/interfaces/provided-service.interface';
-import { ServiceCardComponent } from '../service-card/service-card.component';
+import {Component} from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+import {IProvidedService} from '../../../core/interfaces/provided-service.interface';
+import {ServiceCardComponent} from '../service-card/service-card.component';
 
 @Component({
   selector: 'mvo-service-grid',
@@ -12,10 +12,10 @@ import { ServiceCardComponent } from '../service-card/service-card.component';
 })
 export class ServiceGridComponent {
   providedServices: IProvidedService[] = [
-    { titleKey: 'services.bathrooms.title', descriptionKey: 'services.bathrooms.description', image: 'bathrooms/bathroom4.png' },
-    { titleKey: 'services.tiling.title',    descriptionKey: 'services.tiling.description',    image: 'tile-work/image9.jpeg' },
-    { titleKey: 'services.repairs.title',   descriptionKey: 'services.repairs.description',   image: 'tile-work/image9.jpeg' },
-    { titleKey: 'services.facade.title',    descriptionKey: 'services.facade.description',    image: 'tile-work/image9.jpeg' },
-    { titleKey: 'services.minor.title',     descriptionKey: 'services.minor.description',     image: 'tile-work/image9.jpeg' },
+    {titleKey: 'services.bathrooms.title', descriptionKey: 'services.bathrooms.description', image: 'bathrooms/bathroom4.png'},
+    {titleKey: 'services.tiling.title', descriptionKey: 'services.tiling.description', image: 'tile-work/image4.png'},
+    {titleKey: 'services.repairs.title', descriptionKey: 'services.repairs.description', image: 'reparations/reparation0.jpeg'},
+    {titleKey: 'services.facade.title', descriptionKey: 'services.facade.description', image: 'tile-work/image9.jpeg'},
+    {titleKey: 'services.minor.title', descriptionKey: 'services.minor.description', image: 'minor-solutions/solution0.png'},
   ];
 }
